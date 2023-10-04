d1c25efe66ef05008564866bdc17602d3680781d
Maitreya Save <msave@ncsu.edu>
2023-10-03 20:56:47 -0400

    Added basic logic for randomization and iteratively creating images and lines

--- line_drawings_eyetracking.py
+++ line_drawings_eyetracking.py
@@ -1,29 +1,53 @@
-#libraries
-from PIL import Image, ImageDraw, ImageColor
+# libraries
+import os
+import random
+from math import floor
 
-#image size
-im = Image.new('RGB', (100, 100))
+from PIL import Image, ImageDraw
+
+# Define constants
+cnt = 0
+num_images = 3
+image_size = 120
+num_points = 9
+num_max_lines = 12
+line_size = image_size / 2
+
+# Define paths
+dir = "images"
+if not os.path.exists(dir):
+    os.mkdir(dir)
 
 
-draw = ImageDraw.Draw(im)
-#draw.line((0, 0) + im.size, fill= 0)
-#draw.line((0, im.size[1], im.size[0], 0), fill= 0)
-draw.line((0, 0, im.size[0], 0), width = 10, fill = (255, 255, 255), joint = "curve")
-draw.line((0, 0, 0, im.size[1]), width = 10, fill = (255, 255, 255), joint = "curve")
-draw.line((0, im.size[1], 50, im.size[1]), width = 10, fill = (255, 255, 255), joint = "curve")
+# helper functions
+def draw_basic_line(draw, x1, y1, x2, y2):
+    draw.line((x1, y1, x2, y2), width=10, joint="curve")
 
-im.save('test1.png')
 
-im2 = Image.new('RGB', (100, 100))
-draw2 = ImageDraw.Draw(im2)
-#draw.line((0, 0) + im.size, fill= 0)
-#draw.line((0, im.size[1], im.size[0], 0), fill= 0)
-draw2.line((0, 0, 45, 0), width = 8, fill = (255, 255, 255))
-draw2.line((45, 2, 90, 2), width = 8, fill = (255, 255, 255))
-draw2.line((90, 2, 90, 45), width = 8, fill = (255, 255, 255))
-draw2.line((2, 2, 2, 45), width = 8, fill = (255, 255, 255))
-draw2.line((45, 2, 45, 45), width = 8, fill = (255, 255, 255))
-draw2.line((2, 45, 2, 90), width = 8, fill = (255, 255, 255))
-draw2.line((0, 90, 45, 90), width = 8, fill = (255, 255, 255))
+# loop for each image
+for i in range(num_images):
+    # init
+    num_lines = random.randrange(1, num_max_lines + 1)
 
-im2.save('test2.png')
+    # image details
+    im = Image.new('RGB', (image_size, image_size))
+    draw = ImageDraw.Draw(im)
+
+    # draw each line
+    for j in range(num_lines):
+        point_location = random.randrange(0, num_points)
+
+        row = floor(point_location / 3)
+        col = point_location % 3
+        x1 = 5 + row * line_size / 2
+        y1 = col * line_size / 2
+        x2 = x1
+        y2 = y1 + 20
+
+        print("point: ", point_location)
+        print("row: ", row, "col:", col)
+        draw_basic_line(draw, x1, y1, x2, y2)
+
+    # save image to file
+    im.save(dir + "/test" + str(i + 1) + ".png")
+    print(i + 1, "th image generated")
